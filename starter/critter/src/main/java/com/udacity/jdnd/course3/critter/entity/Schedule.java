4d8e4dde0bbfda56744ed6b078a557e0ff89c342
Hugh Sun <wenjunsun99@gmail.com>
2023-01-18 21:28:58 -0800

    Designed Schedule entity.

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Schedule.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Schedule.java
@@ -1,4 +1,74 @@
 package com.udacity.jdnd.course3.critter.entity;
 
+import com.udacity.jdnd.course3.critter.enums.EmployeeSkill;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Entity
 public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToMany
+    private List<Employee> employees;
+    @ManyToMany
+    private List<Pet> pets;
+    private LocalDate date;
+    @ElementCollection(targetClass = EmployeeSkill.class)
+    @Enumerated(EnumType.STRING)
+    private Set<EmployeeSkill> activities;
+
+    public Schedule() {
+    }
+
+    public Schedule(Long id, List<Employee> employees, List<Pet> pets, LocalDate date, Set<EmployeeSkill> activities) {
+        this.id = id;
+        this.employees = employees;
+        this.pets = pets;
+        this.date = date;
+        this.activities = activities;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Set<EmployeeSkill> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(Set<EmployeeSkill> activities) {
+        this.activities = activities;
+    }
 }
