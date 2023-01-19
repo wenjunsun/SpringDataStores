f32eadc500f3131ea7f6cf015274400eb2ac3620
Hugh Sun <wenjunsun99@gmail.com>
2023-01-18 21:18:54 -0800

    Designed Employee entity.

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Employee.java
@@ -0,0 +1,64 @@
+package com.udacity.jdnd.course3.critter.entity;
+
+import com.udacity.jdnd.course3.critter.enums.EmployeeSkill;
+
+import javax.persistence.*;
+import java.time.DayOfWeek;
+import java.util.Set;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    // This is how you save a collection of enum with JPA annotations.
+    @ElementCollection(targetClass = EmployeeSkill.class)
+    @Enumerated(EnumType.STRING)
+    private Set<EmployeeSkill> skills;
+    @ElementCollection(targetClass = DayOfWeek.class)
+    @Enumerated(EnumType.STRING)
+    private Set<DayOfWeek> daysAvailable;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, Set<EmployeeSkill> skills, Set<DayOfWeek> daysAvailable) {
+        this.id = id;
+        this.name = name;
+        this.skills = skills;
+        this.daysAvailable = daysAvailable;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<EmployeeSkill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Set<EmployeeSkill> skills) {
+        this.skills = skills;
+    }
+
+    public Set<DayOfWeek> getDaysAvailable() {
+        return daysAvailable;
+    }
+
+    public void setDaysAvailable(Set<DayOfWeek> daysAvailable) {
+        this.daysAvailable = daysAvailable;
+    }
+}
